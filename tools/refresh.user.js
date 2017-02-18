4911e4b6a7e06429a0259fc06e18011516981c3d
lgh06 <hnnk@qq.com>
2017-02-18 15:46:08 +0800

    add jQuery,match all url at .user.js

--- tools/refresh.user.js
+++ tools/refresh.user.js
@@ -5,15 +5,16 @@
 // @description  try to take over the world!
 // @author       You
 // @require      http://cdn.bootcss.com/crypto-js/3.1.2/rollups/sha1.js
-// @match        http://m.kankan.com/v/*
+// @require      https://lib.sinaapp.com/js/jquery/2.2.4/jquery-2.2.4.min.js
 // @match        http://127.0.0.1:8080/index.html
+// @match        */*
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 /* eslint-disable */
 (function (){
   'use strict';
   var s = document.createElement('script');
-  s.src = 'http://127.0.0.1:8080/index.js';
+  s.src = 'http://127.0.0.1:8080/index.js?'+Math.random();
   s.async = true;
   document.body.appendChild(s);
 
@@ -26,7 +27,7 @@
         var last = localStorage.getItem('lastscript');
         var str = response.responseText;
         var sha = CryptoJS.SHA1(str).toString();
-        console.log(sha);
+        //console.log(sha);
         localStorage.setItem('lastscript', sha + '');
         if( last != sha )
           window.location.reload();
